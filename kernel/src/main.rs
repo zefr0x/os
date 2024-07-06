ab97daed39a0b335709994d6db574c308006c30b
zefr0x <zer0-x.7ty50@aleeas.com>
2024-07-06 22:17:01 +0300

    kernel(core/start): Initialize physical memory mapping and kernel heap

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
 use bootloader_api::{
     config::{BootloaderConfig, Mapping},
     entry_point,
@@ -19,6 +21,30 @@ entry_point!(start_kernel, config = &BOOTLOADER_CONFIG);
 
 fn start_kernel(boot_info: &'static mut BootInfo) -> ! {
     kernel::init();
+
+    {
+        let physical_memory_offset = x86_64::VirtAddr::new(
+            boot_info
+                .physical_memory_offset
+                .take()
+                .expect("Can't fetch physical memory offset from bootloader."),
+        );
+
+        // Initialize physical memory mapper
+        #[expect(unsafe_code)]
+        // SAFETY: Offset is mapped to valid physical memory and this function is just called once.
+        let mut memory_mapper = unsafe { kernel::memory::init(physical_memory_offset) };
+
+        // Initialize memory frame allocator.
+        #[expect(unsafe_code)]
+        let mut memory_frame_allocator =
+            // SAFETY: Memory regions are valid and all of their frames are usable.
+            unsafe { kernel::memory::BootInfoFrameAllocator::init(&boot_info.memory_regions) };
+
+        // Initalize kernel heap memory
+        kernel::allocator::init_heap(&mut memory_mapper, &mut memory_frame_allocator)
+            .expect("Kernel heap initialization failed.");
+    }
 
     if let Optional::Some(ref mut framebuffer) = boot_info.framebuffer {
         let mut display = kernel::drivers::frame_buffer::Display::new(framebuffer);
